7eaeaaf493332d0b834e9dda7627855b1359a348
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-09-27 22:24:59 -0300

    initial import

---
+++ server.js
@@ -0,0 +1,20 @@
+require.paths.unshift(__dirname + "/vendor");
+
+process.addListener('uncaughtException', function(err,stack){
+	console.log('-------------------');
+	console.log('Exception:' + err);
+	console.log(err.stack);
+	console.log('-------------------');
+});
+
+var LiveStats = require('./lib/livestats');
+
+new LiveStats({
+	port: 9100,
+	geoipServer: {
+			hostname: 'geoip.peepcode.com'
+		,	port: 80
+	}
+});
+
+
